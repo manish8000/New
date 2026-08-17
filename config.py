1f33ebc4434e99e7177995b9cd3dc84bbc08a361
manish8000 <147980408+manish8000@users.noreply.github.com>
2026-08-17 11:43:50 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN", "YOUR_TELEGRAM_BOT_TOKEN")
+GROQ_API_KEY = os.getenv("GROQ_API_KEY", "YOUR_GROQ_API_KEY")
+OWNER_ID = int(os.getenv("OWNER_ID", "123456789"))
